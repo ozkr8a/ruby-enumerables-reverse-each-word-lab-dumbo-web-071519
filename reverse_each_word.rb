496a2022acd5bb785d223581c5f1bb73c70cd380
Oscar <oscar_ochoac@yahoo.com>
2019-07-02 23:14:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def reverse_each_word(string)
   
   sentence = string.split(" ")
@@ -27,31 +26,3 @@ end
   
 #   return result
 # end
-=======
-# def reverse_each_word(string)
-  
-#   sentence = string.split(" ")
-  
-#   new = sentence.collect {|word| word.reverse}
-
-#   print new
-#   new.join(" ")
-# end
-
-def reverse_each_word(string)
-  
-  sentence = string.split(" ")
-  
-  new = []
-  
-  sentence.each do |word|
-    reversed = word.reverse
-    reversed.each_char {|char| new << char}
-  end
-  print new
-  result = new.join()
-  print result
-  
-  return result
-end
->>>>>>> 2202e2b7d6119bf3f0aaefca4534ad8e0f427824
